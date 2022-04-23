fd909a6afd4d5dc25dffb05ef16be739c91aab6d
Riham Mohamed Rabie <rihammohamed2011@gmail.com>
2022-04-24 00:33:13 +0200

    Add files via upload

---
+++ SalesInvoiceGeneratorProject/src/com/sig/view/HeaderDialog.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sig.view;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author RM02206
+ */
+public class HeaderDialog extends JDialog{
+    
+    private JTextField custNameField;
+    private JTextField invDateField;
+    private JLabel custNameLbl;
+    private JLabel invDateLbl;
+    private JButton okBtn;
+    private JButton cancelBtn;
+
+    public HeaderDialog(InvoiceForm frame) {
+        custNameLbl = new JLabel("Customer Name:");
+        custNameField = new JTextField(20);
+        invDateLbl = new JLabel("Invoice Date:");
+        invDateField = new JTextField(20);
+        okBtn = new JButton("OK");
+        cancelBtn = new JButton("Cancel");
+        
+        okBtn.setActionCommand("newAddedInvoiceOk");
+        cancelBtn.setActionCommand("newAddedInvoiceCancel");
+        
+        okBtn.addActionListener(frame.getActionListener());
+        cancelBtn.addActionListener(frame.getActionListener());
+        setLayout(new GridLayout(3, 2));
+        
+        add(invDateLbl);
+        add(invDateField);
+        add(custNameLbl);
+        add(custNameField);
+        add(okBtn);
+        add(cancelBtn);
+        
+        pack();
+        
+    }
+
+    public JTextField getCustomerName() {
+        return custNameField;
+    }
+
+    public JTextField getDate() {
+        return invDateField;
+    }
+    
+}
